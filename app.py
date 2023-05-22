f05d520c21d9ce7d23eb1f91335c284b05d8d471
SparklyGrill <iskra.maradin@gmail.com>
2023-05-22 09:44:21 +0200

    updates
    updated function and style

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template, request
 from weatherapp import main as get_weather
 
 app = Flask(__name__)
+port = 3001
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = None
+    city = None
+    error = None
+
     if request.method == 'POST':
         city = request.form['cityName']
-        data = get_weather(city)
-    return render_template('index.html', data=data)
+        if not city:
+            error = 'Please enter the city name'
+        else:
+            data = get_weather(city)
+
+    return render_template('index.html', data=data, city=city, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
--- weatherapp.py
+++ weatherapp.py
@@ -1,6 +1,7 @@
-import requests
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
+import requests
 from dataclasses import dataclass
 
 load_dotenv()
@@ -12,35 +13,39 @@ class WeatherData:
     description: str
     icon: str
     temperature: int
-def get_lan_lon(city_name, Api_key):
-    #resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q{city_name}&appid={Api_key}').json()
-    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={Api_key}').json()
-    #data = resp
-    #lat = data.get('lat')
-    #lon = data.get('lon')
-    print(resp)
+
+def get_lan_lon(city_name, api_key):
+    resp = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={api_key}').json()
     data = resp[0]
     lat = data['lat']
     lon = data['lon']
     return lat, lon
 
-def get_current_weather(lat, lon, Api_key):
-    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=ab81ca23a1a3029e43740ff17cb0a40e&units=metric').json()
+def get_current_weather(lat, lon, api_key):
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric').json()
     data = WeatherData(
         main=resp.get('weather')[0].get('main'),
         description=resp.get('weather')[0].get('description'),
         icon=resp.get('weather')[0].get('icon'),
         temperature=int(resp.get('main').get('temp'))
     )
-
     return data
 
-def main(city_name):
-    lat, lon = get_lan_lon('Zagreb,hr', api_key)
-    get_current_weather(lat, lon, api_key)
+def display_weather(city_name):
+    lat, lon = get_lan_lon(city_name, api_key)
     weather_data = get_current_weather(lat, lon, api_key)
     return weather_data
 
-if __name__ == "__main__":
-    lat, lon = get_lan_lon('Zagreb,hr', api_key)
-    print(get_current_weather(lat, lon, api_key))
+app = Flask(__name__)
+port = 3001
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    data = None
+    if request.method == 'POST':
+        city_name = request.form['cityName']
+        data = display_weather(city_name)
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=port)
